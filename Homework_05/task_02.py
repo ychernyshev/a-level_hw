47c1695f240dc4208fcb874b2d62e9f20520d696
Eugene <eugene_chernyshev@yahoo.com>
2023-12-28 20:12:47 +0200

    Will be: - complete task 5.3 - Prepare files for tasks 5.2

--- Homework_05/task_02.py
+++ Homework_05/task_02.py
@@ -5,3 +5,40 @@
 #   - студентів, які навчаються у двох та більше групах
 #   - студентів, які не навчаються на фронтенді
 #   - студентів, які вивчають Python або Java
+student = {
+    'Alex': {
+        'Python', 'FullStack',
+    },
+    'Bill': {
+        'FrontEnd', 'FullStack',
+    },
+    'Bob': {
+        'Java',
+    },
+}
+reversed_students = dict(reversed(list(student.items())))
+
+temp_original = set()
+temp_reversed = set()
+result = None
+
+for value in student.values():
+    # temp_original.add(value)
+    temp_original = value
+    # print(temp_original)
+for item in reversed_students.values():
+    temp_reversed = item
+    # print(temp_reversed)
+
+print(temp_reversed)
+print(temp_original)
+result = temp_original.intersection(temp_reversed)
+print(result)
+# result = temp_original.intersection(temp_reversed)
+
+# for item in range(len(student)):
+#     print(item)
+
+
+# print(result)
+
--- Homework_05/task_3.py
+++ Homework_05/task_3.py
@@ -1,3 +1,44 @@
 # Написати функцію переведення розмірів жіночої білизни з міжнародної у решту.
 # Всередині функції потрібно просто звертатися до правильно складеного словника.
+def women_sizes(size):
+    international_sizes = {
+        'XXS': {
+            'UA': 42, 'GE': 36, 'USA': 8, 'FR': 38, 'GB': 24,
+        },
+        'XS': {
+            'UA': 44, 'GE': 38, 'USA': 10, 'FR': 40, 'GB': 26,
+        },
+        'S': {
+            'UA': 46, 'GE': 40, 'USA': 12, 'FR': 42, 'GB': 28,
+        },
+        'M': {
+            'UA': 48, 'GE': 42, 'USA': 14, 'FR': 44, 'GB': 30,
+        },
+        'L': {
+            'UA': 50, 'GE': 44, 'USA': 16, 'FR': 46, 'GB': 32,
+        },
+        'XL': {
+            'UA': 52, 'GE': 46, 'USA': 18, 'FR': 48, 'GB': 34,
+        },
+        'XXL': {
+            'UA': 54, 'GE': 48, 'USA': 20, 'FR': 50, 'GB': 36,
+        },
+        'XXXL': {
+            'UA': 56, 'GE': 50, 'USA': 22, 'FR': 52, 'GB': 38,
+        },
+    }
+    result = ''
+    kv = []
 
+    for isize, sizes in international_sizes.items():
+        if size == isize:
+            for key, value in sizes.items():
+                kv.append(f'{key}: {value}')
+
+    result += ' | '.join(kv)
+
+    return print(' =' * 22, '\n', result, '\n', '= ' * 22)
+
+
+while True:
+    women_sizes(input('Enter the international size: ').upper())
