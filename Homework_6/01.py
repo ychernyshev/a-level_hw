60c98303d4e7d07518a1783a9c1e2fecf9506f1f
Eugene <eugene_chernyshev@yahoo.com>
2024-01-04 08:07:23 +0200

    Will be: - Prototype of solution of the task 6.1

--- Homework_6/01.py
+++ Homework_6/01.py
@@ -1,1 +1,28 @@
 # Знайомимось з zip функцією, пробуємо написати свій власний zip
+def main_zip(first_list, second_list):
+    f_list = []
+    s_list = []
+    result = []
+
+    f_temp = list(reversed(first_list))
+    s_temp = list(reversed(second_list))
+
+    if len(first_list) > len(second_list):
+        shortly_list_len = len(first_list) - (len(first_list) - len(second_list))
+        first_list = first_list[:shortly_list_len]
+    elif len(first_list) < len(second_list):
+        shortly_list_len = len(second_list) - (len(second_list) - len(first_list))
+        second_list = second_list[:shortly_list_len]
+        i = 1
+        for elem1 in first_list:
+            for elem2 in second_list:
+                t1 = first_list[i-1:i]
+                t2 = second_list[i-1:i]
+                result.append((t1, t2))
+                i += 1
+            break
+
+    print(result)
+
+
+main_zip([1, 2, 3, 4], ['go', 'one', 5, 400, True, 'lambda'])
