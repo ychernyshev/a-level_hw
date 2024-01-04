d197b3f4fa156d5bbc2b53fc9ac070c2c1fc18b7
Eugene <eugene_chernyshev@yahoo.com>
2024-01-04 10:42:46 +0200

    Will be: - completed task 6.1

--- Homework_6/01.py
+++ Homework_6/01.py
@@ -1,5 +1,5 @@
 # Знайомимось з zip функцією, пробуємо написати свій власний zip
-def main_zip(first_list, second_list):
+def mine_zip(first_list, second_list):
     f_list = []
     s_list = []
     result = []
@@ -10,14 +10,38 @@ def main_zip(first_list, second_list):
     if len(first_list) > len(second_list):
         shortly_list_len = len(first_list) - (len(first_list) - len(second_list))
         first_list = first_list[:shortly_list_len]
+
+        i = 1
+
+        for elem1 in first_list:
+            for elem2 in second_list:
+                t1 = None
+                for symbol in first_list[i - 1:i]:
+                    t1 = symbol
+
+                t2 = None
+                for symbol in second_list[i - 1:i]:
+                    t2 = symbol
+
+                result.append((t1, t2))
+                i += 1
+            break
     elif len(first_list) < len(second_list):
         shortly_list_len = len(second_list) - (len(second_list) - len(first_list))
         second_list = second_list[:shortly_list_len]
+
         i = 1
+
         for elem1 in first_list:
             for elem2 in second_list:
-                t1 = first_list[i-1:i]
-                t2 = second_list[i-1:i]
+                t1 = None
+                for symbol in first_list[i-1:i]:
+                    t1 = symbol
+
+                t2 = None
+                for symbol in second_list[i-1:i]:
+                    t2 = symbol
+
                 result.append((t1, t2))
                 i += 1
             break
@@ -25,4 +49,4 @@ def main_zip(first_list, second_list):
     print(result)
 
 
-main_zip([1, 2, 3, 4], ['go', 'one', 5, 400, True, 'lambda'])
+mine_zip(['go', 'one', 5, 400, True, 'lambda'], [1, 2, 3, 4])
