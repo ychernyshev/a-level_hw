fba46b1aec2bcae28e62edf92d459063b89b9ed5
Eugene <eugene_chernyshev@yahoo.com>
2024-01-07 19:27:16 +0200

    Will be: - added .idea to the gitignore list - added PyCharm Gitignore List to the gitignore list - improved the code in task 6.1 and in functions_implementation/zip.py

---
+++ functions_implementation/zip.py
@@ -0,0 +1,33 @@
+# Знайомимось з zip функцією, пробуємо написати свій власний zip
+def list_to_symbol_and_to_tuple(first_argument, second_argument):
+    result = []
+    i = 1
+
+    for elem1 in first_argument:
+        for elem2 in second_argument:
+            t1 = None
+            for symbol in first_argument[i - 1:i]:
+                t1 = symbol
+
+            t2 = None
+            for symbol in second_argument[i - 1:i]:
+                t2 = symbol
+
+            result.append((t1, t2))
+            i += 1
+        break
+
+    return result
+
+
+def uzip(first_argument, second_argument):
+    return list_to_symbol_and_to_tuple(first_argument, second_argument)
+
+
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy", "Monica")
+
+x = uzip(a, b)
+
+# use the tuple() function to display a readable version of the result:
+print(tuple(x))
