06c620ab874d157ce0a9d642dc0b662a3f4f3d2f
Eugene <eugene_chernyshev@yahoo.com>
2024-01-14 15:50:42 +0200

    A simple code update will be for sorted function implementation

--- functions_implementation/sorted.py
+++ functions_implementation/sorted.py
@@ -33,32 +33,57 @@ def usorted(line):
     reformated_line_to_list = []
     line_to_indexes = []
     sorted_line_to_indexes = []
+    i = 0
+    result = []
+
+    while i < len(reformated_line_to_list):
+        temp = line_to_indexes[0]
+
+        for item in range(len(line_to_indexes)):
+            if temp > line_to_indexes[item]:
+                temp = line_to_indexes[item]
+
+        sorted_line_to_indexes.append(temp)
+
+        line_to_indexes.remove(temp)
+        i += 1
+
+    for item in sorted_line_to_indexes:
+        for key, value in alphabet.items():
+            if item == key:
+                result.append(value)
 
     if type(line) == tuple:
         for item in line:
             reformated_line_to_list.append(item)
 
+    temp = reformated_line_to_list[0]
     if len(reformated_line_to_list) >= 1:
         for item in reformated_line_to_list:
+            print(temp)
             if type(item) == str:
                 for key, value in alphabet.items():
                     if item == value:
                         line_to_indexes.append(key)
-            # elif type(item) == int or type(item) == float:
-            #     temp = reformated_line_to_list[0]
-            #
-            #     for item in range(len(reformated_line_to_list)):
-            #         print(item)
-            #         if temp > reformated_line_to_list[item]:
-            #             temp = reformated_line_to_list[item]
-            #     result.append(temp)
+            elif type(item) == int or type(item) == float:
+                i = 0
+                # for item in reformated_line_to_list:
+                if temp > item:
+                    temp = item
+                    reformated_line_to_list.remove(item)
+                # print(temp)
+                while i < len(reformated_line_to_list):
+                    result.append(temp)
+                    i += 1
+
+                return result
         return letters_analyzation(reformated_line_to_list, line_to_indexes, sorted_line_to_indexes)
     else:
         pass
 
 
-x = usorted(("b", "g", "a", "d", "f", "c", "h", "e"))
-y = usorted(("u", "v", "z", "w", "y", "x"))
-# x = usorted((1, 2, 5, 4, 3))
-
+print(usorted(("b", "g", "a", "d", "f", "c", "h", "e")))
+print(usorted(("u", "v", "z", "w", "y", "x")))
+x = usorted((1, 2, 5, 4, 3))
+y = usorted((4, 4, 5, 7, 0))
 print(x, y)
